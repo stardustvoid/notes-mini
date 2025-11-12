b0664f55082ee095ba882ff297a330ba3bb60339
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-11-13 02:11:00 +0700

    Add form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,28 @@
 <?php
 
+$config = require('config.php');
+
+$db = new Database($config['database'], $config['dbuser']['username'], $config['dbuser']['password']);
+
 $header = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 500) {
+        $errors['body'] = 'The body can not be more than 500 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+            ':body' => $_POST['body'],
+            ':user_id' => 1
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -13,7 +13,10 @@
                                 <div class="mt-2">
                                     <textarea id="body" name="body" rows="3"
                                               class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                              placeholder="Write note description here..."></textarea>
+                                              placeholder="Write note description here..."><?= $_POST['body'] ?? '' ?></textarea>
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
