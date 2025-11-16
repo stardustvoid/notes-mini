159d856c2a9e69f14cddb98f8399097cc9c6a163
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-11-16 20:32:48 +0700

    Add delete note button

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,17 +4,31 @@ use Core\Database;
 
 $config = require basePath('config.php');
 
+$currentUserId = 1;
 $db = new Database($config['database'], $config['dbuser']['username'], $config['dbuser']['password']);
 
-$currentUserId = 1;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', [
+        ':id' => $_POST['id']
+    ])->findOrFail();
 
-$note = $db->query('select * from notes where id = :id', [
-    ':id' => $_GET['id']
-])->findOrFail();
+    authorize($note['user_id'] === $currentUserId);
 
-authorize($note['user_id'] === $currentUserId);
+    $db->query('delete from notes where id = :id', [
+        ':id' => $_POST['id']
+    ]);
 
-view('notes/show.view.php', [
-    'header' => 'Note',
-    'note' => $note
-]);
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('select * from notes where id = :id', [
+        ':id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view('notes/show.view.php', [
+        'header' => 'Note',
+        'note' => $note
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,6 +6,14 @@
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <p class="mb-6"><a href="/notes" class="text-blue-500 underline">Go back</a></p>
             <p><?= htmlspecialchars($note['body']) ?></p>
+
+
+            <form class="mt-3" method="POST">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button type="submit" class="text-red-500 text-sm">
+                    Delete
+                </button>
+            </form>
         </div>
     </main>
 
