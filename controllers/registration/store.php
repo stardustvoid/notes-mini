5de6fc687e86fcfac357556c09a862172263c39f
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-11-26 03:08:43 +0700

    Hash user password

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -42,7 +42,7 @@ if ($user) {
 
 $db->query('insert into users (email, password) values (:email, :password)', [
     ':email' => $email,
-    ':password' => $password
+    ':password' => password_hash($password, PASSWORD_BCRYPT)
 ]);
 
 $_SESSION['user'] = [
--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h2>Hello! Welcome to the home page!</h2>
+            <h2>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>! Welcome to the home page!</h2>
         </div>
     </main>
 
