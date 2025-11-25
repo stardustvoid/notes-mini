fdbddf156f5fe61a4aa3b9071f86ddf6bec77947
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-11-26 02:37:40 +0700

    Add middleware
    Edit router to support middleware to restrict access to certain routes

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 
 $router->get('/note/edit', 'controllers/notes/edit.php');
@@ -16,5 +16,5 @@ $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
